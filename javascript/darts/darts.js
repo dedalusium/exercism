6a735e696866527c9777094f1d3713800d1b33ee
Nordwin HOFF <nhoff@oui.sncf>
2018-11-13 16:54:54 +0100

    dart solution

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,1 +1,14 @@
-export const solve = (x, y)   => {return 0};
+export default function solve(x, y) {
+  let res = null;
+  const length = Math.sqrt(x ** 2 + y ** 2);
+  if (length <= 1) {
+    res = 10;
+  } else if (length <= 5) {
+    res = 5;
+  } else if (length <= 10) {
+    res = 1;
+  } else if (length > 10) {
+    res = 0;
+  }
+  return res;
+}
