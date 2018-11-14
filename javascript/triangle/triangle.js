aa7dcff3dca4e9679e987464494213f1aa0abbd4
Nordwin HOFF <nhoff@oui.sncf>
2018-11-14 12:02:17 +0100

    Add equilaterals triangle check and illegal side length

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,43 @@
+export default class Triangle {
+  constructor(sideLengthA, sideLengthB, sideLengthC) {
+    this.sides = [];
+    this.sides.push(sideLengthA);
+    this.sides.push(sideLengthB);
+    this.sides.push(sideLengthC);
+  }
+
+  kind() {
+    let type = '';
+    this.checkIllegalsSideLength();
+    this.checkTriangleInequality();
+    if (this.isEquilateral()) {
+      type = 'equilateral';
+    }
+    return type;
+  }
+
+  checkIllegalsSideLength() {
+    this.sides.forEach((side) => {
+      if (side < 0) {
+        throw new Error('Negative sides are illegal');
+      }
+    }, this); // fixme: this param shouldn't be required
+  }
+
+  checkTriangleInequality() {
+    this.sides.forEach((side, index) => {
+      const otherSides = this.sides.slice();
+      otherSides.splice(index, 1);
+      if (side > otherSides.reduce((sum, nextSide) => sum + nextSide)) {
+        throw new Error('Triangle inequality violated');
+      }
+    }, this); // fixme: this param shouldn't be required
+  }
+
+  isEquilateral() {
+    return this.sides.every(side => side === this.sides[0]);
+  }
+
+  isIsosceles() {
+  }
+}
