88fd250a019cb2702341c20ea581cf39817f3236
dedalusium <dedalusium@users.noreply.github.com>
2019-04-11 10:09:00 +0200

    [javascript][resistor-color] solution

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,3 +1,15 @@
-export const COLORS = [];
+const colorMap = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
+};
+export const COLORS = Object.keys(colorMap);
 
-export const colorCode = () => {};
+export const colorCode = color => colorMap[color];
