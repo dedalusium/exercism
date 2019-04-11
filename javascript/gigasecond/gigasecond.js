fbfa22bc3f05461439bcc8a0149450c0810534a1
dedalusium <dedalusium@users.noreply.github.com>
2019-04-11 17:12:03 +0200

    [javascript][gigasecond] full solution submitted

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,1 +1,5 @@
-export const gigasecond = () => {};
+export const gigasecond = date => {
+  const aGigaSecondLater = new Date();
+  aGigaSecondLater.setTime(date.getTime() + Math.pow(10, 9) * 1000);
+  return aGigaSecondLater;
+};
