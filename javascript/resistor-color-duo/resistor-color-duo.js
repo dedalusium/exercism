0b9ab8a30b2c3d231c6895871c6b71092a54cff7
dedalusium <dedalusium@users.noreply.github.com>
2019-04-29 08:59:16 +0200

    [javascript][resistor-color-duo] quick improvement

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,2 +1,9 @@
-export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
-export const value = colors => Number(colors.reduce((accumulator, color) => accumulator + COLORS.indexOf(color), ''));
+export const value = colors =>
+  Number(
+    colors.reduce(
+      (accumulator, color) =>
+        accumulator +
+        ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'].indexOf(color),
+      ''
+    )
+  );
