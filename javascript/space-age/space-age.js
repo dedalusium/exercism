296aaf16bf1ba2e0edb5c0b4c9e5250018d69688
dedalusium <dedalusium@users.noreply.github.com>
2019-06-24 17:25:57 +0200

    [javascript][space-age] test-suite ok

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, ageInSecond) => Number.parseFloat(Number.parseFloat(ageInSecond / orbitalPeriodInSeconds(planet)).toFixed(2));
+
+const PERIODS_IN_EARTH_YEARS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 };
+
+const oneEarthYearInSeconds = 31557600;
+
+const orbitalPeriodInSeconds = planet => PERIODS_IN_EARTH_YEARS[planet] * oneEarthYearInSeconds;
