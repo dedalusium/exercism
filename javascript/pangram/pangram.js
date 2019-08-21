5eea2e2e60f865118ebecdb959af25a7a44b40ed
dedalusium <dedalusium@users.noreply.github.com>
2019-08-21 12:07:44 +0200

    [javascript][pangram] all tests ok

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,5 +2,5 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 export const isPangram = (rawSentence) => {
   const lowerCleanedSentence = rawSentence.toLowerCase().replace(/[^a-zA-Z]/g, '');
   const withoutDuplicates = new Set(lowerCleanedSentence);
-  return Array.from(withoutDuplicates).every((value) => Array.from(alphabet).includes(value));
+  return withoutDuplicates.size === alphabet.length;
 };
