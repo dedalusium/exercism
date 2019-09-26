2e13441c7de4a46bcd72e1181af019bddf90ffe9
dedalusium <dedalusium@users.noreply.github.com>
2019-09-26 15:33:09 +0200

    [javascript][matrix] refactor unecessary class member

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -4,23 +4,22 @@ export class Matrix {
   }
 
   get rows() {
-    const splited = this.rawMatrix.split("\n");
-    this._rows = splited
+    return this.rawMatrix
+      .split("\n")
       .map(rowAsString => rowAsString
         .split(' ')
         .map(c => Number.parseInt(c))
       );
-    return this._rows;
   }
 
   get columns() {
-    this._columns = []
+    const columns = []
     for (let i = 0; i < this.rows[0].length; i++) {
-      this._columns.push([]);
+      columns.push([]);
       this.rows.forEach(r => {
-        this._columns[i].push(r[i]);
+        columns[i].push(r[i]);
       });      
     }
-    return this._columns;
+    return columns;
   }
 }
