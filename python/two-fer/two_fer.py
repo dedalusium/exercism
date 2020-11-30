fe810087772cdaa00b1f7c2ed26acff9872ddb72
dedalusium <dedalusium@outlook.fr>
2020-11-30 21:14:59 +0100

    [python][two-fer] rewrite with ternary

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,2 @@
 def two_fer(name=''):
-    if name != '':
-        return "One for {}, one for me.".format(name)
-    else:
-        return "One for you, one for me."
+    return "One for {}, one for me.".format('you' or name)
