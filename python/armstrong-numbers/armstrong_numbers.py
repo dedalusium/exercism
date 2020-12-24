2bb7baf1a915ac68959606a741288bdc9f396fcc
Nordwin HOFF <nordwin@gensdeconfiance.fr>
2020-12-24 17:52:14 +0100

    [python][armstrong-numbers] every test ok

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
 def is_armstrong_number(number):
-    pass
+    as_string = str(number)
+    size = len(as_string)
+    computation = sum([int(x) ** size for x in as_string])
+    return number == computation
