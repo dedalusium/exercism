7bfdc848c1832cb303048512685fadcaef23638a
Nordwin HOFF <nordwin@gensdeconfiance.fr>
2020-12-25 10:00:53 +0100

    [python][perfect-numbers] add and complete

---
+++ python/perfect-numbers/perfect_numbers.py
@@ -0,0 +1,20 @@
+def split_in_proper_dividers(number):
+    if number <= 0:
+        raise ValueError(number)
+    dividers = []
+    d = number - 1
+    while d > 0:
+        if number % d == 0:
+            dividers.append(d)
+        d -= 1
+    dividers.sort()
+    return dividers
+
+
+def classify(number):
+    aliquot_sum = sum(split_in_proper_dividers(number))
+    if aliquot_sum > number:
+        return 'abundant'
+    if aliquot_sum < number:
+        return 'deficient'
+    return 'perfect'
