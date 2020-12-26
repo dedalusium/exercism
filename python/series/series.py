48088099d5a9703dae10f69b4ad1e5ca3a5b533b
Nordwin HOFF <nordwin@gensdeconfiance.fr>
2020-12-26 16:27:02 +0100

    [python][series] simplify

--- python/series/series.py
+++ python/series/series.py
@@ -2,5 +2,4 @@ def slices(series, length):
     series_length = len(series)
     if length <= 0 or series_length <= 0 or length > series_length:
         raise ValueError(length)
-    return [series[series.index(c, ind):series.index(c, ind) + length] for ind, c in enumerate(series) if
-            ind + length <= series_length]
+    return [series[i:i + length] for i in range(series_length - length + 1)]
