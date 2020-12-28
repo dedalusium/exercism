a7174b5ce1b6f5056a053ac5b46cbe0dc549101d
Nordwin HOFF <nordwin@gensdeconfiance.fr>
2020-12-28 13:48:45 +0100

    [python][high_scores] rewrite with immutability

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,12 +1,11 @@
 def latest(scores):
-    scores.reverse()
-    return scores[0]
+    sorted_scores = list(reversed(scores))
+    return sorted_scores[0]
 
 
 def invert_sort(scores):
-    scores.sort()
-    scores.reverse()
-    return scores[:]
+    sorted_scores = list(sorted(scores, reverse=True))
+    return sorted_scores
 
 
 def personal_best(scores):
