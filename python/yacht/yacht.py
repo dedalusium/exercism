dd0a04c7c2cb24815dd7cb97df81f808391999a5
Nordwin HOFF <nordwin@gensdeconfiance.fr>
2020-12-28 17:18:30 +0100

    [python][yacht] use counters

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,18 +1,5 @@
-"""
-This exercise stub and the test suite contain several enumerated constants.
+from collections import Counter
 
-Since Python 2 does not have the enum module, the idiomatic way to write
-enumerated constants has traditionally been a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
-
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
-
-# Score categories.
-# Change the values as you see fit.
 YACHT = 0
 ONES = 1
 TWOS = 2
@@ -32,24 +19,20 @@ def is_four_at_least(num):
 
 
 def score(dice, category):
-    try:
-        occurencies = [dice.count(n) for n in range(1, 7)]
-        if category in range(1, 7):
-            return dice.count(category) * category
-        if category == CHOICE:
-            return sum(dice)
-        if category == FOUR_OF_A_KIND:
-            is_four = list(filter(is_four_at_least, occurencies))
-            if len(is_four) == 1:
-                return 4 * (occurencies.index(is_four[0]) + 1)
-        if category == YACHT and occurencies.index(5):
-            return 50
-        if category == BIG_STRAIGHT and occurencies.count(1) == 5 and min(dice) == 2 and max(dice) == 6:
-            return 30
-        if category == LITTLE_STRAIGHT and occurencies.count(1) == 5 and min(dice) == 1 and max(dice) == 5:
-            return 30
-        if category == FULL_HOUSE and occurencies.count(3) == 1 and occurencies.count(2) == 1:
-            return sum(dice)
-    except:
-        pass
+    if category in range(1, 7):
+        return dice.count(category) * category
+    if category == CHOICE:
+        return sum(dice)
+    if category == FOUR_OF_A_KIND:
+        nb_repetition = Counter(dice).most_common(1)[0][1]
+        value = Counter(dice).most_common(1)[0][0]
+        return 4 * value if nb_repetition >= 4 else 0
+    if category == YACHT and len(set(dice)) == 1:
+        return 50
+    if category == BIG_STRAIGHT and sorted(dice) == [2, 3, 4, 5, 6]:
+        return 30
+    if category == LITTLE_STRAIGHT and sorted(dice) == [1, 2, 3, 4, 5]:
+        return 30
+    if category == FULL_HOUSE and sorted(Counter(dice).values()) == [2, 3]:
+        return sum(dice)
     return 0
