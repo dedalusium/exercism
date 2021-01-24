fc6337573bb575433f8aaa23ecc0d7dc4fc9fb02
dedalusium <dedalusium@outlook.fr>
2021-01-24 15:50:41 +0100

    [python][hamming] exercise ok

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    strand_length = len(strand_a)
+    hamming_distance = 0
+    if strand_length != len(strand_b):
+        raise ValueError('strand are not equal length')
+    for i in range(0, strand_length):
+        if strand_a[i] != strand_b[i]:
+            hamming_distance += 1
+    return hamming_distance
