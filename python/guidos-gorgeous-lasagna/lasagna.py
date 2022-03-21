30751249946e462e6c1230aa1c3ec3e304a0969f
dedalusium <dedalusium@outlook.fr>
2022-03-21 16:59:50 +0100

    [python][guidos-gorgeous-lasagna] test ok - add poetry in python dir

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,27 @@
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+# TODO: consider defining the 'PREPARATION_TIME' constant
+#       equal to the time it takes to prepare a single layer
+
+
+# TODO: define the 'bake_time_remaining()' function
+def bake_time_remaining(elapsed_bake_time = 0):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+def preparation_time_in_minutes(nb_layer):
+    """Calculate preperation time for a given number of lasagna layer
+    """
+    return PREPARATION_TIME * nb_layer
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate total elapsed preparation time
+    """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+    
